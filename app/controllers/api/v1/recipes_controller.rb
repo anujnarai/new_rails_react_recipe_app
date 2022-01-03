72965f82088f035f6ab5c968d4213369d47b8e62
anujna14 <anujna1396@gmail.com>
2022-01-03 17:34:39 +0530

    added new changes

--- app/controllers/api/v1/recipes_controller.rb
+++ app/controllers/api/v1/recipes_controller.rb
@@ -1,9 +1,18 @@
 class Api::V1::RecipesController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def index
     recipe = Recipe.all.order(created_at: :desc)
     render json: recipe
   end
   def create
+   recipe_params = params[:body]
+   recipe = Recipe.create!(name: recipe_params[:name], ingredients: recipe_params[:ingredients], instruction: recipe_params[:instruction])
+    if recipe
+      render json: recipe
+    else
+      render json: recipe.errors
+    end
   end
 
   def show
@@ -15,9 +24,12 @@ class Api::V1::RecipesController < ApplicationController
   end
 
   def destroy
+    recipe.destroy unless recipe.nil?
+    render json: {message: 'Recipe deleted!'}
   end
 
   private
+
   def recipe 
     @recipe ||= Recipe.find(params[:id])
   end
